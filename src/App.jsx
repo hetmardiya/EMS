d5780e895ffa541c40fca1de58308dbec9c8c6e6
backendworkshore <backend@workshore.io>
2025-03-21 18:55:20 +0530

    starting the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-7xl text-pink-600' >Hellocd </p>
+      <h1 className='text-amber-400'>iam from noone</h1>
+
+    </div>
+  )
+}
+
+export default App
